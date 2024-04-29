bf2032634491c4a4659c09efaee9db0c56462f80
SuprajaMamidi <supraja82002@gmail.com>
2024-04-29 15:49:00 +0000

    nxt trendz 2

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <div>
+    <Header />
+    <div className="image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="image"
+      />
+    </div>
+  </div>
+)
+export default Products
